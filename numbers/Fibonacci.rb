9753a718b67153bfc6ecdd61937e34d6001ab79e
Gabe <glafonta@gmail.com>
2016-05-03 14:24:34 -0400

    fibonacci for ruby

---
+++ numbers/Fibonacci.rb
@@ -0,0 +1,13 @@
+def fibonacci_recursion(number)
+  if (number == 1 || number == 2)
+    1
+  else
+    fibonacci_recursion(number - 1) + fibonacci_recursion(number - 2) 
+  end
+end
+
+puts "Enter a number for the Fibonnaci sequence to print: "
+number = gets.chomp.to_i
+puts fibonacci_recursion(number)
+
+
